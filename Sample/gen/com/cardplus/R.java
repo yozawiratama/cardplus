c4f253df89799128825247f75731b83a6e1809ff
Yoza Wiratama <y_wiratama@yahoo.com>
2013-06-24 17:18:32 +0700

    Weather card sample

--- Sample/gen/com/cardplus/R.java
+++ Sample/gen/com/cardplus/R.java
@@ -50,6 +50,7 @@ public final class R {
     public static final class drawable {
         public static final int cardplus=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int sunny=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f090000;
